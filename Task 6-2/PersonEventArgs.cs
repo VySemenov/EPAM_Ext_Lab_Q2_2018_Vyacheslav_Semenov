a85a55760663e76d25d620e90b3632149bfbba86
Gerzoni <pixelworldofbug@gmail.com>
2018-07-24 11:36:43 +0400

    Task 6 done

---
+++ Task 6-2/PersonEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_6_2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PersonEventArgs : EventArgs
+    {
+        public PersonEventArgs(DateTime time)
+        {
+            this.Time = time;
+        }
+
+        public DateTime Time { get; }
+    }
+}
