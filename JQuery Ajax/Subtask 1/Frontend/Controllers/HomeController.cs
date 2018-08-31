b7648eec2409cd8d7b9a29cf152897e505eb9039
Gerzoni <pixelworldofbug@gmail.com>
2018-08-31 12:17:10 +0400

    Frontend subtask 1 done

---
+++ JQuery Ajax/Subtask 1/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<string> left = new List<string> { "op1", "op2"};
+        List<string> right = new List<string>() { "op3", "op4" };
+
+        public ActionResult Index()
+        {
+            ViewBag.Left = left;
+            ViewBag.Right = right;
+            return View();
+        }
+
+        /// <summary>
+        /// The method moves items from one list to another.
+        /// The list of items to move is stored in the list "change".
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="change"></param>
+        /// <returns>Lists in json</returns>
+        public JsonResult Move(string[] left, string[] right, string[] change)
+        {
+            List<string> leftList;
+            List<string> rightList;
+            if (left == null)
+            {
+                leftList = new List<string>();
+            }
+            else
+            {
+                leftList = left.ToList();
+            }
+
+            if (right == null)
+            {
+                rightList = new List<string>();
+            }
+            else
+            {
+                rightList = right.ToList();
+            }
+
+            if (change != null)
+            {
+                List<string> changeList = change.ToList();
+
+                foreach (var ch in changeList)
+                {
+                    if (leftList.Contains(ch))
+                    {
+                        leftList.Remove(ch);
+                        rightList.Add(ch);
+                    }
+                    else
+                    {
+                        rightList.Remove(ch);
+                        leftList.Add(ch);
+                    }
+                }
+            }
+
+            return Json(new { leftList, rightList });
+        }
+    }
+}
---
+++ JQuery Ajax/Subtask 1/Frontend/Scripts/Mover.js
@@ -0,0 +1,74 @@
+﻿function Move(where) {
+    var rightList = document.getElementById("right");
+    var leftList = document.getElementById("left");
+    var changeList = [];
+
+    if (where == "toLeft") {
+        for (var i = 0; i < rightList.length; i++) {
+            if (rightList.options[i].selected)
+                changeList.push(rightList.options[i].value);
+        }
+    }
+    else {
+        for (var i = 0; i < leftList.length; i++) {
+            if (leftList.options[i].selected)
+                changeList.push(leftList.options[i].value);
+        }
+    }
+
+    if (changeList.length == 0) {
+        alert('Not selected');
+    }
+    else {
+        var left = [];
+        for (var i = 0; i < leftList.options.length; i++)
+            left.push(leftList.options[i].value);
+        var right = [];
+        for (var i = 0; i < rightList.options.length; i++)
+            right.push(rightList.options[i].value);
+
+        console.log({ 'left': left, 'right': right, 'change': changeList });
+
+        $.ajax({
+            url: "Home/Move",
+            type: "post",
+            data: { 'left': left, 'right': right, 'change': changeList },
+            success: (function (json) {
+                console.log(json.leftList);
+                while (leftList.length != 0)
+                    leftList.removeChild(leftList.options[0]);
+                for (var i = 0; i < json.leftList.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = json.leftList[i];
+                    option.text = json.leftList[i];
+                    leftList.appendChild(option);
+                }
+
+                console.log(json.rightList);
+                while (rightList.length != 0)
+                    rightList.removeChild(rightList.options[0]);
+                for (var i = 0; i < json.rightList.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = json.rightList[i];
+                    option.text = json.rightList[i];
+                    rightList.appendChild(option);
+                }
+            }),
+            dataType: "json"
+        });
+    }
+}
+
+function MoveAll(where) {
+    var leftList = document.getElementById("left");
+    var rightList = document.getElementById("right");
+
+    for (var i = 0; i < leftList.options.length; i++) {
+        leftList.options[i].selected = "selected";
+    }
+    for (var i = 0; i < rightList.options.length; i++) {
+        rightList.options[i].selected = "selected";
+    }
+
+    Move(where);
+}
